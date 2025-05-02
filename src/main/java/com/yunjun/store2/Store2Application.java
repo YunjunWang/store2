17b45506aec994457e915537438fbd2e02866f24
Yunjun Wang <yunjun.wangirl@gmail.com>
2025-05-02 16:14:23 +0100

    DB Migration - Use Hibernate to create DB issues: 1. This is a Model-First approach that our model enity class has to main the DB table details otherwise our database schema won't in good shape, which clutters DB design in the entity code 2. When we update our domain model, Hibernate may update the schema in ways that may not be optimized, overtime, our database schema may look really ugly 3. We lose versioning that we can't recreate the exact same database on other machines.
    The Model-First approach is good for quick POC or development. Once our database schema is in good shape, we should use JPABuddy to create the initial DB migration for properly versioning our database.

--- src/main/java/com/yunjun/store2/Store2Application.java
+++ src/main/java/com/yunjun/store2/Store2Application.java
@@ -1,45 +1,13 @@
 package com.yunjun.store2;
 
-import entities.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication
 public class Store2Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Store2Application.class, args);
-        User user = User.builder()
-                .name("test")
-                .email("test@test.com")
-                .password("123456")
-                .build();
-/*      User entity can't add address and tags the same time, get StackOverflow, how to solve it?
-        Address address = Address.builder()
-                .street("street")
-                .city("city")
-                .state("state")
-                .zipcode("zipcode")
-                .build();
-        user.addAddress(address);*/
-
-        user.addTag("tag1");
-        Profile profile = Profile.builder().bio("bio").build();
-        user.addProfile(profile);
-
-        Category category1 = new Category("category1");
-        Product product1 = Product.builder()
-                            .name("product1")
-                            .price(BigDecimal.valueOf(100.00))
-                            .category(category1)
-                            .description("description1")
-                            .build();
-        user.addWishlist(product1);
-        System.out.println(user);
-        System.out.println(product1);
     }
 
 }
