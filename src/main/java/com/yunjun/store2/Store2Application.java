878cf83fc29d34095729ab5a86e18f3471655b61
Yunjun Wang <yunjun.wangirl@gmail.com>
2025-05-02 17:21:51 +0100

    Use Repository

--- src/main/java/com/yunjun/store2/Store2Application.java
+++ src/main/java/com/yunjun/store2/Store2Application.java
@@ -1,5 +1,7 @@
 package com.yunjun.store2;
 
+import com.yunjun.store2.entities.User;
+import com.yunjun.store2.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Store2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Store2Application.class, args);
+
+        var context = SpringApplication.run(Store2Application.class, args);
+        User user = User.builder()
+                .name("John")
+                .email("john@example.com")
+                .password("test")
+                .build();
+        var userRepository = context.getBean(UserRepository.class);
+        userRepository.save(user);
+        userRepository.findAll().forEach(u -> {
+                    System.out.println(u.getEmail());
+                });
+        userRepository.delete(user);
     }
 
 }
