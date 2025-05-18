18c0faca192363207b5d3adc8455a4751c697bcb
Yunjun Wang <yunjun.wangirl@gmail.com>
2025-05-18 09:40:41 +0100

    How basic filter works

---
+++ src/main/java/com/yunjun/store2/filters/LoggingFilter.java
@@ -0,0 +1,30 @@
+package com.yunjun.store2.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+    /**
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("Filter start...");
+        System.out.println("Request URL: " + request.getRequestURL());
+        filterChain.doFilter(request, response);
+        System.out.println("Response Status: " + response.getStatus());
+        System.out.println("Filter end...");
+
+    }
+}
