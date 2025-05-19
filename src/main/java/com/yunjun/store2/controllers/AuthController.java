7f934d680d32aad9db7f02dc5539c9f1721a12f4
Yunjun Wang <yunjun.wangirl@gmail.com>
2025-05-19 21:16:19 +0100

    Spring Security notes

--- src/main/java/com/yunjun/store2/controllers/AuthController.java
+++ src/main/java/com/yunjun/store2/controllers/AuthController.java
@@ -87,4 +87,60 @@ public class AuthController {
         var userDto = userService.getUserById(jwt.getUserId());
         return new LoginResponse(jwtService.generateAccessToken(userDto).toString());
     }
+
+    /**
+     * Client-Side Logout
+     * - Delete access token from the memory / storage
+     * - Delete refresh token by cleaning the cookie
+     * - Simple to implement, but not secure,
+     * - Tokens are still valid until they expire.
+     * - Redirect to the login page
+     * - Better suites for small applications
+     *
+     * Server-Side Logout
+     * - Store a list of active / removed tokens in the database
+     * - When a user logs out, mark the token as invalid in the database
+     * - During each request, check if the token is blacklisted or not
+     * - Provides a true logout experience for large applications
+     * - Adds complexity and requires additional token db lookup for each request
+     * - Best for high-security applications
+     *
+     * TODO: complete this API after course finished.
+     */
+    @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Logout the current user")
+    public void logout() {
+
+    }
+
+    /**
+     * Manual features for security to build a secure application
+     * - Email verification
+     * - Password reset
+     * - Account lockout
+     * - Social login
+     * - Multi-factor authentication
+     * - Multi-device sessions
+     * - Brute-force protection
+     * ...
+     *
+     * Instead, use of the options below to improve security and reduce complexity.
+     * Auth Providers to automate authentication
+     * - Auth0
+     * - Amazon Cognito
+     * - Firebase Authentication
+     * - Okta
+     * Can build and ship apps faster and with fewer security vulnerabilities.
+     * But is expensive.
+     * Use auth provider when:
+     * - Building a startup
+     * - Building an MVP
+     * - If speed matters more
+     *
+     * Roll your own when:
+     * - If operating at scale
+     * - Have specific requirements
+     * - Want full control over the authentication process
+     */
 }
