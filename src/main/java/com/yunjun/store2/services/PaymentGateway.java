82102ebe4b9b41609183d1292bd5c4c699a61942
Yunjun Wang <yunjun.wangirl@gmail.com>
2025-05-28 12:10:42 +0100

    add comment for the PaymentGateway naming convention reason

--- src/main/java/com/yunjun/store2/services/PaymentGateway.java
+++ src/main/java/com/yunjun/store2/services/PaymentGateway.java
@@ -3,6 +3,10 @@ package com.yunjun.store2.services;
 import com.stripe.exception.StripeException;
 import com.yunjun.store2.entities.Order;
 
+/**
+ * name it with the suffix 'Gateway' as it is not a public service,
+ * this service only for internal purpose.
+ */
 public interface PaymentGateway {
     CheckoutSession checkout(Order order) throws StripeException;
 }
